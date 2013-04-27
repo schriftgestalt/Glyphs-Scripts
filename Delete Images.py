80ca45b7b6623bae71a16e4cc93583f674602943
schriftgestalt <georg.seifert@mac.com>
2013-04-27 20:04:48 +0200

    Forgot one line

--- Delete Images.py
+++ Delete Images.py
@@ -1,6 +1,7 @@
 #MenuTitle: Delete Images
 ''' Deletes background images from selected layers.'''
 
+Font = Glyphs.font
 Font.disableUpdateInterface()
 layers = Font.selectedLayers
 for l in layers:
