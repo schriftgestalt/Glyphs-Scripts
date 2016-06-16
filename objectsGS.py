795911874130ab02d8402d831b0fb4c0de116c7a
schriftgestalt <georg.seifert@mac.com>
2016-06-16 13:15:58 -0400

    add HTMLView

--- objectsGS.py
+++ objectsGS.py
@@ -1424,3 +1424,28 @@ class GlyphPreview(VanillaBaseObject):
 		self._setupView(self.nsGlyphPreviewClass, posSize)
 		self._nsObject.setDelegate_(self)
 		self._nsObject.setGlyph_(glyph)
+
+class HTMLView(VanillaBaseObject):
+	"""
+	A view that allows for showing HTML
+	
+	from vanilla import *
+	from objectsGS import HTMLView
+	class HTMLViewDemo(object):
+		def __init__(self):
+			self.title = "HTML View"
+			self.w = FloatingWindow((600, 350), self.title)
+			self.w.Preview = HTMLView((0, 0, 0, 0))
+			self.w.Preview.setHTMLPath("https://www.glyphsapp.com")
+			self.w.open()
+	HTMLViewDemo()
+
+	**posSize** Tuple of form *(left, top, width, height)* representing the position and size of the color well.
+	"""
+	nsHTMLViewClass = WebView
+	def __init__(self, posSize):
+		self._setupView(self.nsHTMLViewClass, posSize)
+		#self._nsObject.setDelegate_(self)
+	def setHTMLPath(self, path):
+		if (path):
+			self._nsObject.setMainFrameURL_(path)
