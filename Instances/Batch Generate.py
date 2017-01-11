453df30d507d024890cb2d88507c80e9abbefc75
schriftgestalt <georg.seifert@mac.com>
2017-01-11 09:49:47 +0100

    add Batch generate script

---
+++ Instances/Batch Generate.py
@@ -0,0 +1,42 @@
+#MenuTitle: Batch Generate Fonts
+# -*- coding: utf-8 -*-
+__doc__="""
+Batch Generate Fonts.
+"""
+
+
+fileFolder = "~/Desktop/files"
+
+otf_path = "~/Desktop/export"
+ttf_path = "~/Desktop/export"
+ufo_path = "~/Desktop/export"
+web_path = "~/Desktop/export"
+
+OTF_AutoHint = True
+TTF_AutoHint = True
+RemoveOverlap = True
+UseSubroutines = True
+UseProductionNames = True
+
+
+import os
+
+fileFolder = os.path.expanduser(fileFolder)
+fileNames = os.listdir(fileFolder)
+
+for fileName in fileNames:
+	if os.path.splitext(fileName)[1] == ".glyphs":
+		font = GSFont(os.path.join(fileFolder, fileName))
+		print font.familyName
+		for instance in font.instances:
+			print "== Exporting OTF =="
+			print instance.generate(Format = "OTF", FontPath = os.path.expanduser(otf_path), AutoHint = OTF_AutoHint, RemoveOverlap = RemoveOverlap, UseSubroutines = UseSubroutines, UseProductionNames = UseProductionNames)
+		print
+		for instance in font.instances:
+			print "== Exporting TTF =="
+			print instance.generate(Format = "TTF", FontPath = os.path.expanduser(ttf_path), AutoHint = TTF_AutoHint, RemoveOverlap = RemoveOverlap, UseProductionNames = UseProductionNames)
+		print
+		for instance in font.instances:
+			print "== Exporting WOFF =="
+			print instance.generate(Format = "WOFF", FontPath = os.path.expanduser(web_path), AutoHint = TTF_AutoHint, RemoveOverlap = RemoveOverlap, UseSubroutines = UseSubroutines, UseProductionNames = UseProductionNames)
+		print
